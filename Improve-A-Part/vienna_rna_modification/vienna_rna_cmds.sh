c6119e551389569f26cfbb281a6356db39bca44b
Emmanouil Anastasios Serlis <manosserlis@gmail.com>
2022-10-11 21:40:28 +0000

    Update Improve-A-Part/vienna_rna_modification/vienna_rna_cmds.sh

--- Improve-A-Part/vienna_rna_modification/vienna_rna_cmds.sh
+++ Improve-A-Part/vienna_rna_modification/vienna_rna_cmds.sh
@@ -1,1 +1,9 @@
+sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install build-essential python python-dev python3 python3-dev python-pip python3-pip
+sudo -H pip3 install setuptools wheel numpy
+sudo -H pip install setuptools wheel numpy
+tar -zxvf ViennaRNA-2.5.1.tar.gz
+cd ViennaRNA-2.5.1
+./configure
+make
+sudo make install
 
